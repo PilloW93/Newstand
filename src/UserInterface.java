6cb4099b800d8e202ae362e7ca89fff53feb1465
PilloW93 <sgrebstad@hotmail.no>
2019-03-11 11:44:50 +0100

    test2

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -97,7 +97,7 @@ public class UserInterface {
         {
             System.out.println(menuItem);
         }
-        int maxMenuItemNumber = menuItems.length + 1;
+        //int maxMenuItemNumber = menuItems.length + 1;
         // Add the "Exit"-choice to the menu
         System.out.println(maxMenuItemNumber + ". Exit\n");
         System.out.println("Please choose menu item (1-" + maxMenuItemNumber + "): ");
